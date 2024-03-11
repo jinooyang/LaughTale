d53a7ba8edbb1610c142c3f7cebe46d59a0cdaa4
jaechan <penguin_03@naver.com>
2024-03-12 01:03:10 +0900

    feat : chapter page design
    Former-commit-id: f8ae4693dc74dc8148cba1a755fa5766de0282df

--- frontend/src/pages/cartoon/Index.tsx
+++ frontend/src/pages/cartoon/Index.tsx
@@ -1,3 +1,3 @@
-const Index = () => {return <div></div>}
-
-export default Index;
+version https://git-lfs.github.com/spec/v1
+oid sha256:931eaba8fc0f1ea07af07c3f71644347e8ed9d2ab099e833a4288836e11cfba8
+size 3593
