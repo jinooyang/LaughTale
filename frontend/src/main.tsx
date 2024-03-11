fca22e459bfa24aada443c5a6f3bbf993f450938
jaechan <penguin_03@naver.com>
2024-03-12 00:59:06 +0900

    feat : add tailwind configuration
    Former-commit-id: dae3587e96d41f956a86856271880ec2f5dd66a2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e749f2d0613b129f994b780261d4015f92cf581da5a7f31ad01e9b11c5853a08
-size 243
+oid sha256:544e804580bac7cfb4d3fc25beb6e688cc3b39e655fc2c543df5bb50c8682be0
+size 263
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:40ba69eddb4672cdc14557106d3d58baa691bdcb2c166d97ef85bd0b735381c0
+size 182
