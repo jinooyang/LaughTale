37afced1895cc680b9cecb4ebb755950a42292c4
yeonmi <ar77gt@daum.net>
2024-03-12 17:01:02 +0900

    design : mypage index
    Former-commit-id: 5cfa8d81b5ce7e28bbf2bb893a27d1830c73db01

--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,15 +1,3 @@
-import Header from "../../components/common/Header";
-import Recent from "../../components/mypage/Recent";
-import LevelTest from "../../components/mypage/LevelTest";
-import BookMark from "../../components/mypage/BookMark";
-
-const Index = () => {
-  return <div>
-<div><Header/></div>
-<div><Recent/></div>
-<div><LevelTest/></div>
-<div><BookMark/></div>
-  </div>
-}
-
-  export default Index;
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6d6bf69c31e9d4cfd8117c0c457f5b15969c2f288f1712a18e23ca4f640ccc9
+size 2458
