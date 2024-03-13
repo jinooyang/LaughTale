afdffbc7326e54d63b0b5300f0463aae6b99ee7c
yeonmi <ar77gt@daum.net>
2024-03-13 13:32:43 +0900

    design: mypage
    Former-commit-id: ce52463105e9537fa8604709c77c29732a90d919

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
-import {router} from "../src/routes/index.tsx";
+import router from "../src/routes/index.tsx";
 import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -1,6 +1,3 @@
-const Index = () => {return<div>
-gegesefsefsefszzzzzzz
-</div>
-}
-
-  export default Index;
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e4807ec592d3a676163fbe5e2d15d8807bdb5f95b564f72a05d1f1f91b800cd
+size 1894
