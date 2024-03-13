a1e801c86c02fb11dcab6f9cb5afe3fee6783f58
Jaechan Kim <penguin_03@naver.com>
2024-03-13 12:44:59 +0900

    fix: route path 수정 및 createRoot 삭제
    Former-commit-id: 081509b933d0afe3a737b7c85e8d89ff3a8b50b3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import router from "../src/routes/index.tsx";
 import "./index.css"
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
 
