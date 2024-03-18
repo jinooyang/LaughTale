7370ce1450a17314ad425e332d396a32badcfc8a
yeonmi <ar77gt@daum.net>
2024-03-18 16:39:14 +0900

    design : quizpage
    Former-commit-id: 8e8d1c5ba9a2d200a4714a8b9e26f141a476c179

--- frontend/src/components/cartoon/Progressbar.tsx
+++ frontend/src/components/cartoon/Progressbar.tsx
@@ -1,12 +1,3 @@
-const Progressbar = ({ value, max }) => {
-    return (
-        <div className="flex justify-center space-x-4">
-            {Array.from({ length: max }, (_, index) => (
-                <div key={index} className={`h-10 w-10 rounded-full text-white font-bold flex justify-center items-center ${
-                    index === value ? 'bg-gradient-to-r from-[#4ba8b9] to-[#6191bf]' : 'bg-[#53525f]'
-                }`}>{index+1}</div>
-            ))}
-        </div>
-    );
-};
-export default Progressbar;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b27b8468f0e958f2ea6f9fcde4008d6f2cce17eff2591a14cfc5df6c11f88544
+size 590
--- frontend/src/components/cartoon/QuizSlider.tsx
+++ frontend/src/components/cartoon/QuizSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52621bb76a48f5018b53655d0cfdb23f8a743dd3748829754190e4bc9d9cb86f
-size 3079
+oid sha256:673abdcb041757a4a3ce1916ed8a0d5a3c2ab546f9b41c1b7401133cfddb9990
+size 3865
