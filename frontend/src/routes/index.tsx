f21fef3672ee04efcb96dba475aae7dc41217bd8
yeonmi <ar77gt@daum.net>
2024-03-18 17:44:21 +0900

    design : quiz
    Former-commit-id: 58a900663ace731eb20430a9d670e2849dd55a09

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd112433c5f90f44b278a21c1d8f3785bd6b7ed3c1c5f4f02f4f96b485200625
-size 1744
+oid sha256:64dc63c4af471810099caa1b1c71c0aef5f2c918f83f3e118b6cf8145002283a
+size 1886
