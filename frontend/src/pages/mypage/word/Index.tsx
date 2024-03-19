a234070fb80e81acc43d52569b795e383c1999d8
yeonmi <ar77gt@daum.net>
2024-03-19 17:46:15 +0900

    design : mypageword
    Former-commit-id: 2c2584e335b246a91c2ea39133b4f560756bc19b

--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a5a0e7788dc34e9ebd5eda7a79a81654ccc59d59b0e6c211141ed6a7b32a28c
-size 949
+oid sha256:95fa26f8a7c54e849d41c63ba1b72ee15322059cd98b49a212eb33c3741cf323
+size 1259
