8e8cbb644085d31d90dd6cb9ec5145950ed5332d
jinooyang <jinooyang@gmail.com>
2024-03-19 12:19:04 +0900

    add : missing entity(quiz, viewhistory, wordbook)
    Former-commit-id: b4344764847532f1bd1652cf02ae707e56bd80ef

---
+++ backend/src/main/java/com/jshi/laughtale/quiz/domain/Quiz.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:16ab9d75095532ddc4c54303e40557618bb49a65b9c31fe3e1841d9d8f2b578b
+size 1069
---
+++ backend/src/main/java/com/jshi/laughtale/wordbook/domain/WordBook.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6b2fe8b2af65e94285457770afc944cc334215b08bfb87879997683efd33245
+size 966
