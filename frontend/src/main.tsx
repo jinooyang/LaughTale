249637a8c0559385d0e122e43fa6802f40aeb08e
yeonmi <ar77gt@daum.net>
2024-03-20 14:27:23 +0900

    merge
    Former-commit-id: 8b9b150ce2fddbacd084fca17dfc1bbbb8503890

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
-import {router} from "./routes";
+import router from "./routes";
 import "./index.css"
 import './styles/SliderStyles.css';
 ReactDOM.createRoot(document.getElementById("root")!).render(
