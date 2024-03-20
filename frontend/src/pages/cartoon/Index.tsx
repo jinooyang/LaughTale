6261b3357110fe1da260eec59f66ddcf8caeb2ce
yeonmi <ar77gt@daum.net>
2024-03-20 16:27:47 +0900

    design : cartoon
    Former-commit-id: 3e2cc174dfd957d1da57d37a1c8633586bf9059f

--- frontend/src/pages/cartoon/Index.tsx
+++ frontend/src/pages/cartoon/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c300b70c73b38d7b013eef325f654805290dc5aabbc2f177e70f3dc645f73b4b
-size 3593
+oid sha256:983bf6bd897a15ffdd068b1e063096d1070f3a9a0ba57f04c1d95043469dea10
+size 4025
