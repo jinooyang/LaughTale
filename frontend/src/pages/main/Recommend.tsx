4720e393b7d59086a43c5b637af7017412f74783
yeonmi <ar77gt@daum.net>
2024-03-20 15:32:21 +0900

    merge
    Former-commit-id: a4939ea1d8b8e6689ca3eb3bb25f11bae473aac7

--- frontend/src/pages/main/Recommend.tsx
+++ frontend/src/pages/main/Recommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28c90e107ffaf3b70ce3cb55143b87340b3695259e850dd115e0a2bc9afaf4fd
+oid sha256:fef9c6adb007ac0c859ce6470c6e1a062fb4694d8bb7eeac9419726b9b19c869
 size 964
--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5457bb9bbadefa9a071ff3ab9a16c898a8c7574da2f8e90c5d83bec1e2550f76
-size 2585
+oid sha256:bb176d0e9406c4ba5e91e2412a64d3a622545877ed6216fc13dea82001065f4a
+size 3911
