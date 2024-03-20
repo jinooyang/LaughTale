2e3e16e7827a67ca98db63587b09731ec39f36f3
yeonmi <ar77gt@daum.net>
2024-03-20 15:32:21 +0900

    merge
    Former-commit-id: bdfcb79803a2049849cdc031bb3d11527b76cda4

--- backend/src/main/java/com/jshi/laughtale/member/dto/MemberCheck.java
+++ backend/src/main/java/com/jshi/laughtale/member/dto/MemberCheck.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3998b21f1e5dbd23ff2e328498fb70aab4e104b55fe2e51ca9527aef4907755b
-size 203
+package com.jshi.laughtale.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class MemberCheck {
+
+    @Builder
+    @Getter
+    public static class Response {
+        String role;
+    }
+}
