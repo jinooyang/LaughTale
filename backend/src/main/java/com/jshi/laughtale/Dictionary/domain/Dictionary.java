189c56fccf3e5e5a6abfee446bb12e5610b25fa8
jinooyang <jinooyang@gmail.com>
2024-03-21 16:35:43 +0900

    feat : Dictionary(japanese word definition)
    Former-commit-id: 77aa536d6092a91d7637513be2020fc579845db2

---
+++ backend/src/main/java/com/jshi/laughtale/Dictionary/domain/Dictionary.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee003070e6ba2a5a9f7769025553267537f258d6f2790c4195b9084f2e0bd35b
+size 647
---
+++ backend/src/test/java/com/jshi/laughtale/InsertWordDefinitionTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69cab35cfbb9fa8b9198b0adaee07ef954f00516fa8b2dc45082a16bb5c1db93
+size 2615
