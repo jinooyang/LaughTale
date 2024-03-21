87f6b3ff01c2c851b7d27d0b0f7e4979dc6868b8
Jaechan Kim <penguin_03@naver.com>
2024-03-21 10:38:09 +0900

    fix: tsconfig 수정 및 main html타입 설정
    Former-commit-id: 21cf2e4c87735495800a60909e227b5419081f11

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d87a2517c13610a88f4b850eb54531612978439a72aa21c9cba46161e5dd957
-size 282
+oid sha256:b8e8158cfc20bd975e344818497df1c7803eea3c8adc66b3032c12a575704d77
+size 295
