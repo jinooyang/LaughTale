6167b496445591dde510c96d9d628a9df60372c8
Jaechan Kim <penguin_03@naver.com>
2024-03-21 10:46:20 +0900

    feature: 뷰어 페이지
    Former-commit-id: 930f444e448025e6416e93c7770ecf1fad596838

--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
-const Viewer = () => {return <div>dfdfdfdf</div>}
-
-export default Viewer;
+version https://git-lfs.github.com/spec/v1
+oid sha256:529d9699fe7730d901bee78b3312aad668d39084f9b15a93c4a6fcd936fe67d5
+size 2277
