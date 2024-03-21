c0ebe826bbfb37bd9bfe69e3c52a20f674297c3c
yeonmi <ar77gt@daum.net>
2024-03-21 17:56:38 +0900

    design
    Former-commit-id: 029e9ad4f44b5da5ca9d3706d755c534939921e5

--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8ec4b3e1f45735931bd8a3f1f5611e61914de05a6796457edda15052be0a5f5
-size 1373
+oid sha256:986fd96934322d78e6b18082955495ba5cae9e91f36c598c4ae2d80df5a15bb9
+size 1232
