44e5889218f30c7e1a2895bbdfb9db2f93d432b4
jinooyang <jinooyang@gmail.com>
2024-03-21 16:48:54 +0900

    rebase pull : resolved merge conflict
    Former-commit-id: 0894c2f08f68bd840554f3b4f73832c54bfa5f52

--- backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac3ac44755425246a23f4cbc409648fc876c8879a86c0ee1e92ef8ebbb6eeff
-size 519
+oid sha256:34799a66c2dc59b1ed03e7a79a5fa42c456ff06b5a53a83883c6e532122e3fa9
+size 526
