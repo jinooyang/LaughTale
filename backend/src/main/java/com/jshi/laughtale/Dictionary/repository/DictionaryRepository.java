bb07eef722d1cc80d7bc31db8c29757b9fdddc35
jinooyang <jinooyang@gmail.com>
2024-03-22 14:45:49 +0900

    remove : Dictionary
    Former-commit-id: 35f5bb7a38a38b0f73954d8dade3b0069b3b6a89

--- backend/src/main/java/com/jshi/laughtale/Dictionary/repository/DictionaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jshi.laughtale.Dictionary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jshi.laughtale.Dictionary.domain.Dictionary;
-
-public interface DictionaryRepository extends JpaRepository<Dictionary, Long> {
-
-}
