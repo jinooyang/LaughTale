2d9ed55cee68ee6432baee11b1f839170a7cd08c
dohyeong <ehgud619@naver.com>
2024-03-22 17:29:24 +0900

    feature:만화 등록 버튼 추가 및 썸네일 버그 수정
    Former-commit-id: f6dab62a9c045be2085ff31a16b742bf8efe2276

--- frontend/src/components/admin/FileUploadComponent.tsx
+++ frontend/src/components/admin/FileUploadComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53539b291a19aef85c8135033b952f106c6a16a2a35733938e6699de99bc8c1
-size 3776
+oid sha256:d3a74235533e1e6e6c154d7ec8eaaada3280fdecbbbd6e0a65105b7aa4bf38f8
+size 4039
--- frontend/src/pages/admin/Index.tsx
+++ frontend/src/pages/admin/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa4801a352d3fb288b0fc8bb154f9b03c68b231c56681a1a80cb669d60165829
-size 1746
+oid sha256:ff3f96b1243dc0631c374e53c5c026bb81ddd31fb5cb3e092947d0ccd47bf6dc
+size 3090
