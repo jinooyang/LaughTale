c342c92d26347102a6b60becd4a3bd365ed9b397
jinooyang <jinooyang@gmail.com>
2024-03-22 16:03:03 +0900

    feat : chapterController, getMangaChapters
    Former-commit-id: 1a43bcb688606d7b98c97b408e4c447dcc3b5877

---
+++ backend/src/main/java/com/jshi/laughtale/chapter/controller/ChapterController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42be613d11677b5e849e2dc91c35ec7c6c16e17db34a232bd46e45decfb46d6e
+size 1144
--- backend/src/main/java/com/jshi/laughtale/chapter/repository/ChapterRepository.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/repository/ChapterRepository.java
@@ -1,7 +1,12 @@
 package com.jshi.laughtale.chapter.repository;
 
 import com.jshi.laughtale.chapter.domain.Chapter;
+import com.jshi.laughtale.manga.domain.Manga;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChapterRepository extends JpaRepository<Chapter, Long> {
+	Page<Chapter> findAllByMangaOrderByChapterNoAsc(Manga manga, Pageable pageable);
 }
