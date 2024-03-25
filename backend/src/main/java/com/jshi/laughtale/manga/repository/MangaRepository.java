9c40cac586ece922d7347ce9c659e5c8763ce549
jinooyang <jinooyang@gmail.com>
2024-03-25 12:23:33 +0900

    fix : manga leve
    Former-commit-id: a392319ed3f91464c9283163c04c43560ae194da

--- backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
+++ backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eed7ee0434b310014099445985d34dccd328907c4c917b85197d715b1e5397f0
-size 1825
+oid sha256:9d629c5c4eab19a4f31b28fecb941991a73fbcbd25ec9dae97e45e3e652e348d
+size 1229
