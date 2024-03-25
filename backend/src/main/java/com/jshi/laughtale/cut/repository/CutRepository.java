590b2e88010ecd510e0ddc351ec2e985d6eab753
yeonmi <ar77gt@daum.net>
2024-03-25 10:30:19 +0900

    feat : rebase
    Former-commit-id: 9c9979968515dfd095456efba83f08401c0915d7

--- backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
+++ backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c336c0a7bc11d05d709a45d394570f3835c3b16c85cddd6581f066e04e7e8d9
-size 569
+oid sha256:b2d9e812f5df3f7ce3f77db396c3df1c3b3d2d2f740116c44be73f8f0fb0434f
+size 519
