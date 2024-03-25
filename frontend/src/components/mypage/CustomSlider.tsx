18c11ac210b0553d14e81d30a5548983c7558eed
yeonmi <ar77gt@daum.net>
2024-03-25 17:17:52 +0900

    design : mypage
    Former-commit-id: 9ba5898db2f4c183449e4f69a7dcead258532809

--- frontend/src/components/mypage/CustomSlider.tsx
+++ frontend/src/components/mypage/CustomSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac34243a73e13f5daeee04e2637a0c864deb4285c99dc9e47620a922a9cef4e3
-size 9733
+oid sha256:362872120311638175b78fac02b1f00126700a7d02c9964a74779b6cb4a7eb2f
+size 9734
