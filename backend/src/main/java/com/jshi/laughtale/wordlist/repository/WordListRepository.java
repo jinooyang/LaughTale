dd6362bdf52390d8e0a0bb889ee7b269b645a261
jinooyang <jinooyang@gmail.com>
2024-03-27 08:24:24 +0900

    fix : makeQuiz(added sentence and definition)
    Former-commit-id: b54991c1ee6785e865889e888799dfafa989a877

--- backend/src/main/java/com/jshi/laughtale/wordlist/repository/WordListRepository.java
+++ backend/src/main/java/com/jshi/laughtale/wordlist/repository/WordListRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85cb1b0700b046b6dbb1ba192761a5e9b88caad01f52a63934ccd84ebbbae603
-size 2390
+oid sha256:6f6fcd6de6485ac5aa29325d81ef814a6045cb301f400e2584d0054d74cc8ff6
+size 2615
--- backend/src/main/java/com/jshi/laughtale/wordlist/service/WordListService.java
+++ backend/src/main/java/com/jshi/laughtale/wordlist/service/WordListService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e77e1f61ed3820e8a8b7d8b8d4c83af0eeb56af0e7b13ba8cdf09bda9185778f
-size 1442
+oid sha256:e3f9041abdb505db889fa598d87f52504615243cd1e59dadfaae4633c1ef3000
+size 1548
