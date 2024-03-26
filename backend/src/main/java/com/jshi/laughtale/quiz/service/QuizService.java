e5b12cff93eae41f933154a6bfd56f82fd093cab
jinooyang <jinooyang@gmail.com>
2024-03-26 12:41:22 +0900

    test/make quiz
    Former-commit-id: 013272d5682c0524cde7406f8b842adc2e2cf49f

--- backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:809476147a835c5050255b6ac4cba06da17ab0149465e68208d0de1d266b56ac
-size 6768
+oid sha256:01b881556635cff85059ae7c3ca308e48d2167d578cb19d5fda52bce498e733e
+size 7112
