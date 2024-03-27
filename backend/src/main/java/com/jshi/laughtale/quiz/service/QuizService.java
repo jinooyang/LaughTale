fea899b23cb8e436d9a2f6565de1b36f1b165dce
jinooyang <jinooyang@gmail.com>
2024-03-27 10:01:07 +0900

    fix : makeQuiz(added answerNo to DB)
    Former-commit-id: 15d622f7a23ca4594572a3717a4b7f385f1f07cd

--- backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01b881556635cff85059ae7c3ca308e48d2167d578cb19d5fda52bce498e733e
-size 7112
+oid sha256:0208da01f36f44f77c9cc4b08cd8784f5b82fc5fb39c591fc04e1dab6d30d8f3
+size 7151
