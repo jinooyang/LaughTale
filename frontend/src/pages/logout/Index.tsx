626d3419f3387c15f7339b4eb3807bfa23ac03ef
jaechan <penguin_03@naver.com>
2024-03-27 21:21:31 +0900

    feat: logout test전
    Former-commit-id: 0f0dde800d85e3e11f06a8f90188696901e27cf5

---
+++ frontend/src/pages/logout/Index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6ec464f685dbd6a12bbcd23ece7a38abd12f0b2c89b39008a4a8bd0fcc082c1
+size 223
--- frontend/src/stores/useLocalStorage.ts
+++ frontend/src/stores/useLocalStorage.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797c89e54727a5dac5694f721cbd3266f83a26e6688f23e342c85d0f334f8ec3
-size 281
+oid sha256:0deff9f4efe8dfce434cc37c0c60a0527c8629afefc008529720a74e808a5c18
+size 368
