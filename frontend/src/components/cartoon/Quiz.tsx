6c0cd24c27b9917e5062f6f549f1affb181116be
yeonmi <ar77gt@daum.net>
2024-03-27 17:16:19 +0900

    feat : api
    Former-commit-id: 7b9bfdc359043555db1fe9912f02019799b4b0bd

--- frontend/src/components/cartoon/Quiz.tsx
+++ frontend/src/components/cartoon/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:518df1ebf03afa64e6870feba0b2bd5d95362d6d2b1e490124231d1785274471
-size 1952
+oid sha256:e281ded76b8d43e821989f656c545ede506b0c1a93587170accad69b1530e5e5
+size 1378
