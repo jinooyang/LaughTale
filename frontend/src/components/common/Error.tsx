b5ff1f8cd56a15ca49863cf83b87873c3dc50032
Jaechan Kim <penguin_03@naver.com>
2024-03-27 10:12:41 +0900

    feat: auth button
    Former-commit-id: 548325ef678c8c2e5cd3b2585dcd87d331d084ce

--- frontend/src/components/common/Error.tsx
+++ frontend/src/components/common/Error.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99bd218465d2fe9b4f2338d03298f12fb74b1d9ae6d2232eefb8acba726102e8
-size 109
+oid sha256:637030c03f5a43d52a540cfe255f1f3860a90100ce301c3a8df8017a3666dff6
+size 108
