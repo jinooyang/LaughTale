62546935ba3d1e8171a618a1a337da144f92f281
jaechan <penguin_03@naver.com>
2024-03-27 20:52:33 +0900

    feat: 타입추가
    Former-commit-id: e2fdc42f805b5e3e383b8f949c75754e41b1802b

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,15 +1,3 @@
-
-export type Token = {
-    accessToken: string;
-    refreshToken: string;
-}
-
-export type Role = "USER" | "ANONYMOUS" | "ADMIN";
-
-export type User = {
-    id:number;
-    profile:string;
-    nickname: string;
-    role: Role;
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef0b30ac10790eaeb234e2d42f039dcdb6cd05ab0d8a23d8e0a7e75c36c4307e
+size 247
