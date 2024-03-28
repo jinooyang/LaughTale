da387d503a02fd97e816bf83c502c4b729a32484
Jaechan Kim <penguin_03@naver.com>
2024-03-28 12:32:14 +0900

    fix: 충돌 해결
    Former-commit-id: eccac4b9380945fc1636081b8c687f6a913009a6

--- frontend/src/pages/cartoon/MangaInfoFetchingSuspense.tsx
+++ frontend/src/pages/cartoon/MangaInfoFetchingSuspense.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef1d36e44e5c83ea402519cd6cbb0c47bb46ace334c2b4670b442df6674beb9f
-size 424
+oid sha256:f97a740eff3ba1e5461ae3495afbeedc55264b6bbe5fa12dbb1256d0e0f02697
+size 261
--- frontend/src/pages/login/Index.tsx
+++ frontend/src/pages/login/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fccb617dde700a586e82304929ed5f89705c8e2439eb3f90c6643b3fb121139f
-size 492
+oid sha256:995aa07f4f9d534b054e1ffa389c6433a90f427798db7c270258ad8eb4ce95a7
+size 449
