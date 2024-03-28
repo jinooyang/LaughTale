aa253b6fd64631cd6a55f4046ba213a896e51b14
Jaechan Kim <penguin_03@naver.com>
2024-03-28 15:46:03 +0900

    feat: position 관련 타입 추가
    Former-commit-id: c61ef6d8ca57b421ea5374edfe148dc764ce70a6

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef0b30ac10790eaeb234e2d42f039dcdb6cd05ab0d8a23d8e0a7e75c36c4307e
-size 247
+oid sha256:179fd54bb8abf1376fd41158cce1cbf736c78289aafdbae458ba30ac1b446fd1
+size 841
