d0a33b0dc8ff19d147d541304e9380132054f180
jinooyang <jinooyang@gmail.com>
2024-03-28 15:35:21 +0900

    fix : wordListRepo and make new quiz api url
    Former-commit-id: 752e4858282aeb59165d0162af972a551197f24b

--- backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/service/QuizService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:291ac86233610795d78d9b7dc26b4083f6f6eb166c37a31a6b9ea40fb0cedfda
-size 7173
+oid sha256:033dae6c58679e15ef046e67e8c6d5ad77c076a053112de88e8da6e4678c7437
+size 7267
--- backend/src/main/java/com/jshi/laughtale/wordlist/repository/WordListRepository.java
+++ backend/src/main/java/com/jshi/laughtale/wordlist/repository/WordListRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c2d5241bc366141469153a9c2211fd8d36af466d07c89179eee27e07c8421dd
-size 2689
+oid sha256:83a8dcacb00c118e66ce9a765a69dad256271c6a25ba49ef5c17f9d1c5a97bd4
+size 2695
