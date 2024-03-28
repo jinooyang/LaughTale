cc24b1c01eacd0699a0648ff7af10d0d0a1cd7d3
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-28 18:02:32 +0900

    fix: 단어장 등록 및 삭제, 단어 자세히 보기, 말풍선에 있는 단어 불러오기 오류 해결
    Former-commit-id: 23ba243ac03cb40bcb5bb80a906c7e0e553a8506

--- backend/src/main/java/com/jshi/laughtale/speech/dto/SpeechBasic.java
+++ backend/src/main/java/com/jshi/laughtale/speech/dto/SpeechBasic.java
@@ -1,15 +1,3 @@
-package com.jshi.laughtale.speech.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-public class SpeechBasic {
-
-    @Builder
-    @Getter
-    public static class Response {
-        private Long id;
-        private String sentence;
-        private String translate;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:74bab8aa6ecfd46b1c982ff7de48992c8b71ad94b1b2e219157f85929842be10
+size 240
--- backend/src/main/java/com/jshi/laughtale/wordlist/controller/WordListController.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a1d854d0d28dbe2eefffeb18c6c419ddaa294a1eff091e0d11163f701c17d3f8
-size 908
