69be639589c453ac2cde9bd46e39fe1d6b40a907
yeonmi <ar77gt@daum.net>
2024-03-28 18:02:02 +0900

    feat : mypage
    Former-commit-id: 4d225eef70b705304a30c9243e7142612e3513cb

--- frontend/src/components/cartoon/getQuizInfo.tsx
+++ frontend/src/components/cartoon/getQuizInfo.tsx
@@ -2,7 +2,7 @@ import client from "../../apis";
 // 퀴즈 정보를 가져오는 함수
 
 const getQuizInfo = async (quizId) => {
-    const response = await client.get(`/quiz/${quizId}`);
+    const response = await client.get(`/quiz/new/${quizId}`);
     return response.data;
 };
 export default getQuizInfo;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd8a05e8f7915df2b7cd219b31954a33c0e01e34649aae5d01a0bd5d09f8193d
-size 2266
+oid sha256:291c7ae5f905a8739005e98c2ff0b39889a4b00c21d158af9a27297f2701b9f8
+size 2270
