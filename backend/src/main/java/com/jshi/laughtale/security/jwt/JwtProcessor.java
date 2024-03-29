f7f441e73408f4ec48657d53617093aae25367ee
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-29 15:15:08 +0900

    fix: duration 수정
    Former-commit-id: b8f0c95e6abd1b9757c108f5981e9bee854951cd

--- backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
+++ backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62285bb537e5b6ce94b1a42c706b2d0b8e60f7e5386a661ddf615f55e65b34fd
-size 2182
+oid sha256:243a44635cba81d9c504adad8ea5e63ffa058b63deef7d053121bcec565bb484
+size 2183
