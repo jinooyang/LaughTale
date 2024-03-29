d4da23c336b9ec427dd1f2f159eb38f4dbbf4f8c
dohyeong <ehgud619@naver.com>
2024-03-29 14:59:45 +0900

    fix : fix admin
    Former-commit-id: f8b7a23ef250528b8174fc394c1945bae1a4ca71

--- frontend/src/pages/admin/Index.tsx
+++ frontend/src/pages/admin/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a565f4b9d07dc52b1d4a7776c5dfb016594d827d2d48f5a46aee3afe1cf2dcb7
-size 3196
+oid sha256:0795c6c51bb87e05bdb44f941164abd7b5d8a13ae4d18fc439d66001de36cb67
+size 3660
