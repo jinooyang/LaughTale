417670950d0407eeb1d5cf2116edaec691b94849
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-29 14:24:04 +0900

    fix: upload 로깅 추가
    Former-commit-id: 5e59f28ed62bb8aeacbd26ece216d39659e7ec42

--- backend/src/main/java/com/jshi/laughtale/manga/controller/MangaController.java
+++ backend/src/main/java/com/jshi/laughtale/manga/controller/MangaController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:065207a8d037daae7b51a9288320b5d7ae860873fbdcd475a9ff127b005afea1
-size 2879
+oid sha256:d98ab89bfbe55456fc561ab3cf53f53edfeea8c3094b4bfb0d0851f9d4159f2e
+size 3038
