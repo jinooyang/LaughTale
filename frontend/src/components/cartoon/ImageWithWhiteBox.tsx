da17dbf072f6f6db93891b6d7fe660a3d5e93b43
yeonmi <ar77gt@daum.net>
2024-03-29 10:42:59 +0900

    merge
    Former-commit-id: fcb7eec41589747587f535b3dbffe34a96fe50ad

--- frontend/src/components/cartoon/ImageWithWhiteBox.tsx
+++ frontend/src/components/cartoon/ImageWithWhiteBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3e905133955f2d02e94dcd935712002eb043e2ebc8b5201c6dc09ca508e28de
-size 1901
+oid sha256:25706e8d6bf9a103cfc57069c74a8e7c0095419d7855556ba58278cbff3227fe
+size 2243
