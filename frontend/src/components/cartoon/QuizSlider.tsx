40bfd904f229c64d87c87e3d7ee30cfa7c862f62
yeonmi <ar77gt@daum.net>
2024-03-29 10:05:33 +0900

    merge
    Former-commit-id: 6bb3d5b273f0476703028343ef02ac48531b4b2e

--- frontend/src/components/cartoon/QuizSlider.tsx
+++ frontend/src/components/cartoon/QuizSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7999a50dda0673aecdfcd734e1c353271e57d6f4fd262f57a99025cb663fae1a
-size 3697
+oid sha256:61938d0a76c4afea2c259540275d464051e67f84279b123c1f6b293dcb332fd7
+size 4113
