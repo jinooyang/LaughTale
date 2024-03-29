b0666d40a608c3fe0e013ac8f1c1c5c2900410f2
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-29 13:57:40 +0900

    fix: oauth2 redirect
    Former-commit-id: ef690b582e89a515e58bb138ace302a8e3548195

--- backend/src/main/java/com/jshi/laughtale/security/handler/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/jshi/laughtale/security/handler/OAuth2SuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5eb6b4032d8b2a6b55247b1c464b2530dc3471c12382c3259ba2d400c943ac3
-size 1844
+oid sha256:548f776c8a31aa85909dcbe749140cf9d8544cb16a1efb13f20fcaea5906b6ee
+size 1849
