cbf7fde1780686d7cbe3a207b63eecf4e191293c
Jaechan Kim <penguin_03@naver.com>
2024-03-29 13:10:57 +0900

    fix: builderror
    Former-commit-id: faf9a1a6e7b785c25bb86bcc08a162d4e10f1367

--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c89f2890966c0fd3fd8d973dec131e7263f1f28c743ddae8748bf777fd9776f3
-size 4578
+oid sha256:40d5df161bbb7416156f9333c884745b599fdae52bd99850bdc3ec9d5459a412
+size 4628
