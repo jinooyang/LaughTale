7c1d183ecc5c17467d05db586b84e89f6508d044
yeonmi <ar77gt@daum.net>
2024-03-29 13:09:44 +0900

    feat : quizpage
    Former-commit-id: 905cf5bc646c0b8d403972eb919ecf9fc48160ae

--- frontend/src/components/cartoon/Quiz.tsx
+++ frontend/src/components/cartoon/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99eaf9920d8109ac911637554d16e747a116166b0f79d93e488d8fb83ca723cd
-size 1378
+oid sha256:2f7e2762572bc12670b46eb81652428c94d996eefcacfb52d1e03b4cd20318d1
+size 1412
