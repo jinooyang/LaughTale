74adb8dfbbbe24f3cc964489d1f9199443603660
yeonmi <ar77gt@daum.net>
2024-03-31 00:53:59 +0900

    design : mypage
    Former-commit-id: a0e9eedc334b97e2e37c1c3571ff98c54743a1cf

--- frontend/src/components/mypage/getWordInfo.tsx
+++ frontend/src/components/mypage/getWordInfo.tsx
@@ -3,6 +3,7 @@ import client from "../../apis";
 const getWordInfo = async (level, page, size) => {
     try {
         const response = await client.get(`/word-book/${level}?page=${page}&size=${size}`);
+        // console.log(level, "레벨이 무엇인가?");
         return response.data;
     } catch (error) {
         console.error("API 호출 중 에러 발생:", error);
