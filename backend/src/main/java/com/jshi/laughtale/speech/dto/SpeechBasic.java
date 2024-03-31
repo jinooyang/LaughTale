be37f0bd3f566f77b06858878509fdd9d73e25c3
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-31 15:48:10 +0900

    fix: 단어 나온 컷 이미지, 좌표 응답 추가
    Former-commit-id: 9252fd324ccbfe0e8b2f442b9965933bb95a5951

--- backend/src/main/java/com/jshi/laughtale/speech/dto/SpeechBasic.java
+++ backend/src/main/java/com/jshi/laughtale/speech/dto/SpeechBasic.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74bab8aa6ecfd46b1c982ff7de48992c8b71ad94b1b2e219157f85929842be10
-size 240
+oid sha256:a62574cd86b42fec874467ccaee22cb353d1865b533a68ff4e8bd1ddf5cc3e91
+size 376
