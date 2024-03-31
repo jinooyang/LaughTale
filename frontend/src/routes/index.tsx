21e86064fd980982e80bb2e232f733b1d67a319b
jinooyang <jinooyang@gmail.com>
2024-03-31 22:51:34 +0900

    feat : new-main-page
    Former-commit-id: cde0b819607f0adb9b121e2807521f3e12c2a4cf

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85980a618e878614dc28dafcc47d3fd45716f989e64567b9982a63f96ba64fb2
-size 2244
+oid sha256:036791762417d64dcc3f05a3ac940c2aa02ba8bfb31f6bfb595b4ee96400e8b1
+size 2351
