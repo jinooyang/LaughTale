0e26e4209f794dbf15b81e8d57392c52ffd4f33a
jinooyang <jinooyang@gmail.com>
2024-04-01 01:16:45 +0900

    design : new main page
    Former-commit-id: 7aa15ec84270e5a4b56499e8e2ca2e0f7fb8825c

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b226326cd4c18cafc543d09d39bd2437f77744528b685fe843b482a157b2bae
-size 5685
+oid sha256:56bbdb8c95cf055bad31ac87a537b9f70de2e9b76c315972abe8279274869759
+size 9871
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb2a64f630a694f208eeca8e38d06811d76a67e4532dc0c48189bf7a1a814b68
-size 261
+oid sha256:aeb08f36c2d3308294381c9be293cfb65ad6f8c5c9e4dfc6f893a19f5849c6dc
+size 1671
