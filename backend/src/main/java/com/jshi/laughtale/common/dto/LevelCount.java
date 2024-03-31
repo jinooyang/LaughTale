c8ca7b37ae1bd0fd479113f26a416315dc88066b
seunghw2 <seunghw2@gmail.com>
2024-03-31 19:44:17 +0900

    feat : 하나의 chapter에 들어있는 단어들을 난이도별로 분류하는 api 구현
    Former-commit-id: 5c9fa286dee663c9fdb4dd97251de6b98eaee636

---
+++ backend/src/main/java/com/jshi/laughtale/common/dto/LevelCount.java
@@ -0,0 +1,21 @@
+package com.jshi.laughtale.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class LevelCount {
+	@Getter
+	@Builder
+	public static class Request {
+
+	}
+
+	@Getter
+	@Builder
+	@Setter
+	public static class Response {
+		private int level;
+		private long count;
+	}
+}
