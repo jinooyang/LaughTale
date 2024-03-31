cec07aba12a3a8ca577ca2d876bfc7db3df49465
seunghw2 <seunghw2@gmail.com>
2024-03-31 20:25:44 +0900

    feat : Chapter에서 단어가 하나도 없는 level의 경우 count : 0으로 되도록 변경
    Former-commit-id: 96e6d0cf09557fc0e572fbeab538a64302af0c35

--- backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:056a83a5f07f757c70ecfa08e10246b853f416c81a6bb9cc2cb9a991b3654557
-size 4056
+oid sha256:4a29a5ef6722a68fae6a86f56a7b897ed5d0685eaa1abfc28d5fcf62992bfbc5
+size 4481
