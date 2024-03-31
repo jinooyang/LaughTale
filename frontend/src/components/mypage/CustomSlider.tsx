c127a8a445effcec7334caaa3d5dce5684ab7a8f
yeonmi <ar77gt@daum.net>
2024-04-01 00:58:58 +0900

    feat : mypage the end
    Former-commit-id: 0082a0881e87aea4305dea10324c1e53b494660f

--- frontend/src/components/mypage/CustomSlider.tsx
+++ frontend/src/components/mypage/CustomSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34f5955820b81425a6dd4723be0e48d8d1a3c1f968d6a39ce0c88c3b5e7ba364
-size 12802
+oid sha256:544b81ed97ecfc341ad74cc927f25009ea3ed7bade46613c7c717ac83b5f9f86
+size 11121
--- frontend/src/components/mypage/getWordExample.tsx
+++ frontend/src/components/mypage/getWordExample.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66913ae3c661ca01cfee2ec5f1bd6457f4a4a364b52170e927f1d2b56f438376
-size 436
+oid sha256:ef60b064b53a0ec88587422e3d190aa061dddf4c18ec64f43c6c2e1c0409d338
+size 386
