5d6f4efba5cddbae9113a82dd7a5a52a3957bc28
seunghw2 <seunghw2@gmail.com>
2024-03-31 17:51:39 +0900

    feat : 기존 DB에 있지만, level이 부여 안된 manga들 level update
    Former-commit-id: 4b37a2fedb5afbb1152c55b4f9543d0ade3a2193

--- backend/src/main/java/com/jshi/laughtale/manga/domain/Manga.java
+++ backend/src/main/java/com/jshi/laughtale/manga/domain/Manga.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdb0f41d055be4b489076ede590a3a0cdd409280987aa6c971410c74d853c663
-size 1281
+oid sha256:6bdd1a321c0dd45af756754c4fa7abe9060456885d93916cdf9af5ca53e55868
+size 1293
--- backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
+++ backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1fddf4a3e34ceb55b427886aada5bcec2ce9e95228f14e86c43cc3bd68bf4c6
-size 4179
+oid sha256:9a3093c4522f33fa26b2b6424b5b2ccdf94ca0b7bb3835df3d498427b1b892d7
+size 4756
