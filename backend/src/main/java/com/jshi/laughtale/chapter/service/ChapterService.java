ebf61241695188d33117d606a94e3214178d2538
seunghw2 <seunghw2@gmail.com>
2024-03-31 17:44:12 +0900

    refactor : @Transactional 제거 (@PostStruct로 인해 적용 안 됨)
    Former-commit-id: 65a1833ba7c08629494c2df90ad8742cb35fc44c

--- backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:673441df7024e78a53dbf7db722fd74530011278a37cd64e7a79446b7a558192
-size 3508
+oid sha256:dc27218ff346d97405de00d1e2b0f6c28a18f1d43aee302d62625a47e2d6870a
+size 3489
