0c037191af08bd519246d2f9c27a16595452965f
jinooyang <jinooyang@gmail.com>
2024-04-01 16:11:45 +0900

    push
    Former-commit-id: 513872c60e3a17e2bedb9597b5cc5efa6147c814

--- backend/src/main/java/com/jshi/laughtale/worddata/controller/WordDataController.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/controller/WordDataController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77da45ebe1c409272c101fbd83e068b49480721721ee976f38f63bc21e65eb2a
-size 1410
+oid sha256:ce6885d60d1d86d31fa46c3401d05b2d63abffc3104ac75c84492ec5837a580e
+size 1416
