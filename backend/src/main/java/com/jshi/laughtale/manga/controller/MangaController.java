db6615b36d4119fd714ea7763f0e0466f54fe589
정승환 <seunghw2@naver.com>
2024-04-01 15:22:47 +0900

    feat : manga의 모든 챕터들을 level 별로 개수를 제공하는 api 구현
    Former-commit-id: 1f2985233320bec57cf6588df0ba937b31d5e333

--- backend/src/main/java/com/jshi/laughtale/manga/controller/MangaController.java
+++ backend/src/main/java/com/jshi/laughtale/manga/controller/MangaController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c50a516fcd394b77b591eb3a99aa689c18d2a92f79b62199b34f9054f4dcf402
-size 3412
+oid sha256:3cbca7c0500e0f0e99fe2a9a4149b2e3403cfbccd33c4aba773ee46aadf791ec
+size 3681
