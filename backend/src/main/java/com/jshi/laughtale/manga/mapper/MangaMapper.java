33a1ca5ac3a378f7430789452696af80de25906b
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-01 12:57:55 +0900

    fix: 사용자 사진 보여주기 위한 로직 수정
    Former-commit-id: 3bec8792bfa656a302fab9127e9a8e034272ab56

--- backend/src/main/java/com/jshi/laughtale/manga/mapper/MangaMapper.java
+++ backend/src/main/java/com/jshi/laughtale/manga/mapper/MangaMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a13fc633c42b4401ad84063977c3966798c5a6bcab1f91e101eea3a7bc65e17
-size 2386
+oid sha256:d142c69a56e89532aa5940a833b297741710be9f883c7979d36a82ac7bafb486
+size 2505
