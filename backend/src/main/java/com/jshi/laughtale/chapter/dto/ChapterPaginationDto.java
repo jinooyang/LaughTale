21cdb0e4cefc716cfbaac665ab54ceefbf16d65c
Jaechan Kim <penguin_03@naver.com>
2024-04-01 15:25:46 +0900

    fix: page number error
    Former-commit-id: 71a0001a81ba0091a5811362fb5a5c774cfbc421

--- backend/src/main/java/com/jshi/laughtale/chapter/dto/ChapterPaginationDto.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/dto/ChapterPaginationDto.java
@@ -1,14 +1,3 @@
-package com.jshi.laughtale.chapter.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-public class ChapterPaginationDto {
-
-    @Getter
-    @Builder
-    public static class Response {
-        private Integer nextPage;
-        private Integer prevPage;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:59b5aeaa3b639d7a5913843919c8872d29b62fd9f6c8fba3adc2adda8c6fd45d
+size 254
