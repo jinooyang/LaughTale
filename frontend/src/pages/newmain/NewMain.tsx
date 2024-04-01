f20e1c65f0da9457704df5b87e7e72aac3339873
jinooyang <jinooyang@gmail.com>
2024-04-01 13:13:21 +0900

    add : font
    Former-commit-id: ebbfa4e49613f4a3f5037b034282de2d15b9c5c3

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cba9db64138dcaabc6fa827e3c7d4a70282b1a7226c8334eed5c4ad5d4d39792
-size 14343
+oid sha256:618eb28870956cc721faccbedd44c34cd169554b500f4b3217ac28cc91131668
+size 14338
