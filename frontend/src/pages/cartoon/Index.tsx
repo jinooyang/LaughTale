05541e243be8858b9bdfece39a6d7a6ff5eb61c8
정승환 <seunghw2@naver.com>
2024-04-01 15:08:03 +0900

    feat : donut graph - manga word level
    Former-commit-id: 838bb20485cbbf7e97d55149dfdcc08ff45f7b06

--- frontend/src/pages/cartoon/Index.tsx
+++ frontend/src/pages/cartoon/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50634b0e8ddc4ec804f1303fa762a0bbb70d676a82e8d2e5b515daf4aa517798
-size 3041
+oid sha256:5a77f75447d49de677c430116a2672aa3276ca43d6731c45b86bf3dbfa6ba3ca
+size 5077
