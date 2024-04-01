3e069b6323776c479f54c6e01d07f200e3efbfb4
yeonmi <ar77gt@daum.net>
2024-04-01 12:43:18 +0900

    feat : delete api, prepage,nextpage
    Former-commit-id: 60920308ab4c185cfb045acc0a886d52ebe59428

--- frontend/src/components/cartoon/QuizSlider.tsx
+++ frontend/src/components/cartoon/QuizSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a330c03da16eb5d89de925c630f249f97d7706ae48800363cccce6f0e0d744a5
-size 9557
+oid sha256:af76b2c77bee67978ac5f41430305fbd90f2cb21ca35dcebb4da8e7ce22bdb9b
+size 9775
---
+++ frontend/src/components/mypage/getChapterInfo.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:469a4f8fc6bed7a6510bf0036a495dedc599b7be077161923c0845dbdcb4d32e
+size 393
