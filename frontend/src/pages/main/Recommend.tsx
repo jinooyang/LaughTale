ac14ff571df0d623c866a8ff52570b41db73652f
yeonmi <ar77gt@daum.net>
2024-04-02 08:57:44 +0900

    design : mainpage
    Former-commit-id: 979b109294402ba70fc2a0e819947d3cd91bc50d

--- frontend/src/pages/main/Recommend.tsx
+++ frontend/src/pages/main/Recommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:964dee8e1beba5986df5ac8f980ea9f5091cb5718d5a7335a6cd6ba090f296fa
-size 1979
+oid sha256:f5fc994ceab8cd21127d6fb54fe189520995f95be6c58acb5a19d465523ab285
+size 2062
