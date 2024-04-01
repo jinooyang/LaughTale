01a2ca4cae55c372cb997d305dbc1ac4d378fac7
jinooyang <jinooyang@gmail.com>
2024-04-01 22:07:50 +0900

    feat : btn in new main
    Former-commit-id: 99e8e7096f9cbac7e9f3e19875096879aec024d2

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886a9b05c18b1921cad0a4b2d0ad10e29cae373f2654287a46275133cd90aceb
-size 12051
+oid sha256:11adfc26530476a41d163f2f3842006e4632c6ee33df46fed0e252b8e0af42d4
+size 12060
