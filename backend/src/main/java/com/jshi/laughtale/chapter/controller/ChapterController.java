87ced73c330480e0ba6423a87ba4b1a05a92c512
정승환 <seunghw2@naver.com>
2024-04-01 13:46:38 +0900

    fix : manga의 모든 단어들을 level 별로 개수를 제공하는 api 수정
    Former-commit-id: 79e0c188b28179d683aec9f7b1562539740db44e

--- backend/src/main/java/com/jshi/laughtale/chapter/controller/ChapterController.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/controller/ChapterController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8461f0a04fab971d9f74cd4438d921d9d5038aed277b99f1a9293829ebd2ce65
-size 1933
+oid sha256:50e5ece7f92d910d2733a74aa21a71ca42ec1897afed40b513afe42bce7396c8
+size 1868
