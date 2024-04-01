ab0bda2df7262d9f5bdb038f77d47a1e63522e63
jinooyang <jinooyang@gmail.com>
2024-04-01 22:24:25 +0900

    fix : header logo navigation, feat : newmain button navigation
    Former-commit-id: 89646f13ffb2788e825c4bb2edb2bcf9c9c9aa76

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11adfc26530476a41d163f2f3842006e4632c6ee33df46fed0e252b8e0af42d4
-size 12060
+oid sha256:f881837e1f2ec9628878aae1a4f88a7eb20cf18104324cc22481e9b2d4b1f060
+size 12356
