56d86237746da36e0556f064069721f296fc019c
Jaechan Kim <penguin_03@naver.com>
2024-04-01 12:34:19 +0900

    fix:type error
    Former-commit-id: d5f674c1b68e454bfdace01bc88f6b4ec0b8642c

--- frontend/src/components/cartoon/QuizSlider.tsx
+++ frontend/src/components/cartoon/QuizSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:188dcbb68e52e8334b7024abbd3c1a964df3efeab7d34fd421393e3098a83ef1
-size 9287
+oid sha256:a330c03da16eb5d89de925c630f249f97d7706ae48800363cccce6f0e0d744a5
+size 9557
