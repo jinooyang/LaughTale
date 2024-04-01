fbcb8de1315dc41bc7b0a1bb12833ed92802897d
yeonmi <ar77gt@daum.net>
2024-04-01 15:12:32 +0900

    design : color
    Former-commit-id: 2c454561ffd167937cffdb7a2c9623c681593e9d

--- frontend/src/components/cartoon/ResultWord.tsx
+++ frontend/src/components/cartoon/ResultWord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a541b647a41da787b2e621b6f469b83f04e5ba4a0b37cc0fb1027a3e35080684
-size 4712
+oid sha256:311953a203dcd3b81f2e1192c5b5140782bd7810bb3c416bea18383ca5208ff3
+size 4708
