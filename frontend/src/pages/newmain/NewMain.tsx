5a0ba2aa9fdc53b3ad32b35396ef5348a3c2617f
jinooyang <jinooyang@gmail.com>
2024-04-01 14:32:51 +0900

    design : mainpage
    Former-commit-id: 8cc1e055d8ef414242b05f32f18ff33195357144

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:618eb28870956cc721faccbedd44c34cd169554b500f4b3217ac28cc91131668
-size 14338
+oid sha256:4c28682d4df291b9fa7937e393818224f936b59f5fa623fd61d3ab117f2c3565
+size 9818
