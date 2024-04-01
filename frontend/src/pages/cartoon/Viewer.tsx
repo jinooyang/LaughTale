5a538f24ac2abc56ee1ed94cb8e026ccf3b269f4
Jaechan Kim <penguin_03@naver.com>
2024-04-01 10:13:50 +0900

    update: 이미지 인피니트 스크롤 추가
    Former-commit-id: f3fdfeb3bbbdffbf017d69ed58a6c251fda9d8a0

--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc2c5df15c2ad18f2c711dd4a50385c21bbd61c84e1017cb6b571e613cb1d22e
-size 3217
+oid sha256:304708e709b01b584b9525b5e71328226f6d1ec8006a712712058d688e6fb7a0
+size 4309
