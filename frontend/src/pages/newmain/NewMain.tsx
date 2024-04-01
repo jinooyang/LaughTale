e20cfb2db0022558ee43228f7d41253bae9568f4
jinooyang <jinooyang@gmail.com>
2024-04-01 22:34:33 +0900

    fix : newmain button move to analyze
    Former-commit-id: 280f7027815e2dfcc908abeeb80b670545d3c3bc

--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f881837e1f2ec9628878aae1a4f88a7eb20cf18104324cc22481e9b2d4b1f060
-size 12356
+oid sha256:f4a5093e3d9a19a761eb3d92cda5bdaadc4b2e3d5d6d332cab8875bd3b39524d
+size 12359
