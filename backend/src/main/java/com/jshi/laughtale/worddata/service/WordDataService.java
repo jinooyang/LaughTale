59685795f5269672fe11fca4645353832b58ad9d
jinooyang <jinooyang@gmail.com>
2024-04-01 15:16:27 +0900

    refactor : worddata/{id}  returns only top ten
    Former-commit-id: 569afa8fb7086d168d7b6bb75de8af39f6e8c42b

--- backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9046a967979a4bea718f407d89413f48f9504c5701607ea9cec6dbac0f95a140
-size 2487
+oid sha256:a4df7ef96b5a39050ff8aad3cd534e6c79c0942feefcb1aaaa13bd3b9db1215c
+size 2749
