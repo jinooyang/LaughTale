b3e8cdd9f8ca1796d441266a2750f9df731852d3
jinooyang <jinooyang@gmail.com>
2024-04-02 11:02:00 +0900

    feat : font
    Former-commit-id: 76ca9c2b86039d7e2204b1164a898ccc20e6f46f

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2b146b13b9e90240de84e52ddb7725959727aac003b194e05b93b0e8b3eb786
-size 664
+oid sha256:da00108baee1aa07c0da83d62bfffcb2a88c3247b713f0f6b0031188b318c0ff
+size 697
--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4a5093e3d9a19a761eb3d92cda5bdaadc4b2e3d5d6d332cab8875bd3b39524d
-size 12359
+oid sha256:55732e8d141f01a30e503b654e8fc4424ceb55edaa3a7d1e73e696fea4cebda9
+size 12355
