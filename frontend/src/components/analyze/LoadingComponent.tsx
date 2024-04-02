2e6960948cabcddddb48b5d796f845e29ae90fba
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-02 10:40:10 +0900

    fix: 업로드 대기 화면
    Former-commit-id: d9bcee16d7b338d9c03ea34c680a9962a06c18ad

--- frontend/src/components/analyze/LoadingComponent.tsx
+++ frontend/src/components/analyze/LoadingComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f61635f0bddac59048ed0d5f4a9078d6042f82a1c87c952c48696325f4178fed
-size 2322
+oid sha256:421fc7265435bb4c194f7edbecaea785f358fa48cd6ed060148cb8ed221fd5ea
+size 5903
