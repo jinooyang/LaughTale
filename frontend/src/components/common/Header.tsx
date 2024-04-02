a5a172aa1507d94c00555f1fad12e94b80e923f2
정승환 <seunghw2@naver.com>
2024-04-02 17:37:33 +0900

    design : header 위치 변경
    Former-commit-id: 52a01d9cd66b78ff71c60d593db075065851f23d

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eef209062ed062b128af74ab527f2edab987b25e60dda5badd54f39e980f2b8
-size 1501
+oid sha256:f5d6266bc9ffdce1ea3dd8c374e6428b12db5b206b294c309b8f1e400abcf8bd
+size 1495
