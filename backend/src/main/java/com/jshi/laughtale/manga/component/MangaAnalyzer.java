573baa9ed062150cfe07d8995f66f1facc23f160
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-02 14:13:42 +0900

    fix: analyze response 단어 level 5로 고정 되는 오류 수정
    Former-commit-id: 06f13d8547f8eba552417a46789157600197134c

--- backend/src/main/java/com/jshi/laughtale/manga/component/MangaAnalyzer.java
+++ backend/src/main/java/com/jshi/laughtale/manga/component/MangaAnalyzer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed66f23730f55b37ecfaa4e95ca32d32851728a86335b5bf33f7b0bed9e73262
-size 6449
+oid sha256:de82e40e182c1d0ff1301465be33e545f80e0eb54bea197031c1c121ff19b70e
+size 6645
--- backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c53165a72a781be536db2f8318d3db2ea5cfdab51cfe05e8907cb904b3eee1
-size 3229
+oid sha256:90a6b6d84cfe8509fe716fa0eca26ad5956209e5eadfc8511c91b77de07d1967
+size 3375
