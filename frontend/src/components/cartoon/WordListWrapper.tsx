404de82a38547fdd7f444b5b17233a2b60b320a6
Jaechan Kim <penguin_03@naver.com>
2024-04-02 10:54:32 +0900

    feat viewer 디자인 수정
    Former-commit-id: 782dea26ea494f5166edd4c875e7cd8924939ecd

--- frontend/src/components/cartoon/WordListWrapper.tsx
+++ frontend/src/components/cartoon/WordListWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:329a02d37391b1d3fb48576fb8a54ead7e96d50c5e7267fdde24d524afc6369f
-size 543
+oid sha256:675e7a6f55eedccb436d60e22755c9cc0d0da121af2972f19b38011049a5945a
+size 515
--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:201fbc9d7ce7c23060e929fef46a235c6b4923eed7e5f9820a838cdd218b4321
-size 4337
+oid sha256:f58da2e64494af7b290b817d3946acee5fbcf6015905c6eddab241eccb82dc6c
+size 4531
