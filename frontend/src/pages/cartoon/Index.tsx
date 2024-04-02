57c3b59bb3c1cb512184e0c2e8356561b09b0558
jaechan <penguin_03@naver.com>
2024-04-02 21:21:01 +0900

    feat: chapter pgae design
    Former-commit-id: 8d96b6148fac3e56005c197ad9740efc9662ee35

--- frontend/src/pages/cartoon/Index.tsx
+++ frontend/src/pages/cartoon/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01a67a67d7dca4f198137f2cf70122b0f17df0c1b40344350369177ce0690843
-size 5984
+oid sha256:923aaf099efe3c07978d94daa4201be3f1c11f0cf8cea9583ed3c0d79e15ab4b
+size 6601
