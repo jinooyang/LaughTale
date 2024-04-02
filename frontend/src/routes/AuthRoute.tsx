36cb99d5e97f6919c37da2e1dcd8058d5eddc8bb
Jaechan Kim <penguin_03@naver.com>
2024-04-02 15:45:44 +0900

    fix : buildError
    Former-commit-id: 3cec9bc8ffcbba8c07db72c39ccd95eb07fac442

--- frontend/src/routes/AuthRoute.tsx
+++ frontend/src/routes/AuthRoute.tsx
@@ -1,9 +1,3 @@
-import {useAuth} from "../stores/useAuth";
-import {Navigate, Outlet} from "react-router-dom";
-
-const AuthRoute = () => {
-  const token = useAuth((state) => state.token);
-
-  return token ? <Outlet/> : <Navigate to="/login" />
-}
-export default AuthRoute;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c980aa2bb40233d7e3e903b6b2860f65bd11cb414ec7ee5919dd01015d853977
+size 361
