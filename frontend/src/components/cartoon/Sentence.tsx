53459cd6f0122dd3f5eeb8f6b21ef3267a9ed389
Jaechan Kim <penguin_03@naver.com>
2024-04-02 10:57:18 +0900

    feat viewer 디자인 수정2
    Former-commit-id: b40c9abefe494a6f491f8bf561efaabc401af358

--- frontend/src/components/cartoon/Sentence.tsx
+++ frontend/src/components/cartoon/Sentence.tsx
@@ -6,7 +6,7 @@ type Props = {
   originSentence : string;
 }
 export default function Sentence(props: Props){
-  return <div className="sticky top-0 bg-white p-5 flex">
+  return <div className="sticky top-0 bg-white p-5 flex z-50">
     <div dangerouslySetInnerHTML={{__html:props.sentence}} className="text-6xl text-black"></div>
     <SpeechButton sentence={props.originSentence} style={{width:"5rem", display:"inline"}}/>
   </div>;
