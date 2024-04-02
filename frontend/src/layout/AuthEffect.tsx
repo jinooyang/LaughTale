4c5b591c01ad0b391c3cafead320fb98f38cd7ca
Jaechan Kim <penguin_03@naver.com>
2024-04-02 14:53:05 +0900

    feat: auto auth test
    Former-commit-id: 1a522c6bf968ce9a9de2b90ce8e0255e5ca9fb66

--- frontend/src/layout/AuthEffect.tsx
+++ frontend/src/layout/AuthEffect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6964941cb237e3d4fe259ceb96b8d8f4fd3039ff9f4dfa84b8aa1150ee71f4e
-size 1080
+oid sha256:4b141636f78cb851bf9d375562256ad437f2c3ab1341f4b09f167861c81a7320
+size 1063
