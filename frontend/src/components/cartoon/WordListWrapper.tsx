26e7db11efaa8621948808e665fdf12acce19542
Jaechan Kim <penguin_03@naver.com>
2024-04-02 13:18:50 +0900

    update: header position
    Former-commit-id: a63bc931c69a44c33b242eaac536345936dc119b

--- frontend/src/components/cartoon/WordListWrapper.tsx
+++ frontend/src/components/cartoon/WordListWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:675e7a6f55eedccb436d60e22755c9cc0d0da121af2972f19b38011049a5945a
-size 515
+oid sha256:3e8b96ff15431fb0efad6c6ca91d50cf6dc960918c2522961e74fc2c437691a9
+size 519
