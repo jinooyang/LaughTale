a5280164ed0543eb4a266782a960a353b4882503
정승환 <seunghw2@naver.com>
2024-04-02 09:32:16 +0900

    feat : main page에 캐러셀 api 구현
    Former-commit-id: 4c282cbbc001ab470e6c652b07c95f10c1acbf10

--- frontend/src/components/main/RecentWord.tsx
+++ frontend/src/components/main/RecentWord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cbe9bf164527e5a775f0dad87311e3dcc2d5b5e17217d853dda279e0c924be8
-size 3692
+oid sha256:81c1b35251eef0e324535866f300b6a76556d9ad3a8a9083cd5d6b87e5f5d46c
+size 3615
