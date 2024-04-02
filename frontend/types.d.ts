044012de632f438a5439492a2439fcb561ecacad
Jaechan Kim <penguin_03@naver.com>
2024-04-02 14:43:24 +0900

    update: header 수정
    Former-commit-id: f3611765c913aacff73e94b7002776f763a5bd10

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43df8eda559a8e7f24cd499f270003aacaea17f56ce497c20f799b8d9fcd0541
-size 1558
+oid sha256:849cb52e5c6f84df17d19a0eef8ad204318c33c5a6e3d77f1099e5361c026be8
+size 1567
