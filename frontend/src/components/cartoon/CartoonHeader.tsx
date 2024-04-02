4775569d2f12719d7e4ff42fa720d89f0b22803a
jaechan <penguin_03@naver.com>
2024-04-02 21:36:12 +0900

    feat: 첫번쨰 화 넣기
    Former-commit-id: d8c8f5f360a7dd6049bbffab4a30bff880a14d86

--- frontend/src/components/cartoon/CartoonHeader.tsx
+++ frontend/src/components/cartoon/CartoonHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893c5a06f3fbb8ad44be915da98e685746718adaa44a4f808db13d6b57936c90
-size 1865
+oid sha256:4dbc7060249b9dcf8b0cfceb18a51a4af69e0c5a8a00c6d49c3366e9a6833e2e
+size 1878
--- frontend/src/pages/cartoon/manga/FirstEpisode.tsx
+++ frontend/src/pages/cartoon/manga/FirstEpisode.tsx
@@ -1,11 +1,12 @@
 import {NavLink} from "react-router-dom";
 
 type Props = {
-  mangaId: number
+  mangaId: number;
+  chapterId: number;
 }
 export default function FirstEpisode (props: Props){
-  const {mangaId} = props;
-  return (<NavLink to={`/cartoon/${mangaId}/viewer/0`} >
+  const {mangaId, chapterId} = props;
+  return (<NavLink to={`/cartoon/${mangaId}/viewer/${chapterId}`} >
     <div
       className="text-2xl font-semibold text-center p-4 bg-gradient-to-r from-[#64BEE2] from-5%   to-[#8395E8] to-100% mt-10 mb-5 rounded-full">
       첫화보기 1화
