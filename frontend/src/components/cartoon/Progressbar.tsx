0e49e244bad1a0c269396f8bc68fc856e8db975d
yeonmi <ar77gt@daum.net>
2024-04-03 21:18:08 +0900

    merge
    Former-commit-id: 311d1ce808d5d114d3e019a6cfb30183abfcc0f6

--- frontend/src/components/cartoon/Progressbar.tsx
+++ frontend/src/components/cartoon/Progressbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e4e9c960edd7cae63de6c44dca2723779acbbbcd6201dca184682da3c60ba1b
+oid sha256:6ad64283aa9d1092ef1bc8005eeba0d66e5d43b20e4f5c0145f44c3c29934095
 size 596
