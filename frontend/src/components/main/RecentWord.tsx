6143730a05a35c238afa2aa51197588155fe4598
jinooyang <jinooyang@gmail.com>
2024-04-03 14:28:44 +0900

    fix : design recent word(scale down)
    Former-commit-id: 793155b536f1b01abd854f6d1e1adb30e9678c3d

--- frontend/src/components/main/RecentWord.tsx
+++ frontend/src/components/main/RecentWord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ffdafda9ab06c8680f9f7a7118948e67acfacd742817d42df4cdae205334701
-size 4129
+oid sha256:e4d98495d9571b6f1fe19554af6828e6ea08695d71894c738293c01cf26f4e61
+size 4141
--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:108a2f9430a54cde97aae9877343d4260f6b8f4b223cfc74dd090e6bb711f072
-size 9443
+oid sha256:7764bdd74a01e03368f1322dd629e00dc5853aca9cb96e5d453dd3a87bb6ec2f
+size 9356
