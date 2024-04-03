7386521031a6ef025dc852deae8ce03d311a38b7
jinooyang <jinooyang@gmail.com>
2024-04-03 17:18:38 +0900

    design : definition background color
    Former-commit-id: cff3f97bbd6b2496f5edf2d2a4d0fba0f2489458

--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1baf9d2ff659f4a670264f2c038ea2aecadcac785a860edd95f92427c1982740
-size 4708
+oid sha256:0bdb1a6d9eb837a73f6346df25531e4b44b5e7ac6ff9954c70cdef9ae6273bf0
+size 4760
