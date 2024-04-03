ab70aad86e0c180272c96c8242a1b695875dff7a
Jaechan Kim <penguin_03@naver.com>
2024-04-03 21:34:53 +0900

    feat add close button
    Former-commit-id: ede8b38bfe66b6de103c722f817db28526d2ccb1

--- frontend/src/components/cartoon/WordList.tsx
+++ frontend/src/components/cartoon/WordList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3109f899eaa1bd2d958a07decae572901eeff6a4e4665579bb065a369c57ffe
-size 1574
+oid sha256:b3cd5a5de02e5e03f3feae851b33bdcc84d3371ad663bdb1cac35bb317cbd968
+size 1576
--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:087b0e72aeae2fc96d54ec5c649b42391e966b7d501e1e62e7d7bd82d7427b28
-size 4935
+oid sha256:57679331968da8208ce851016b528ae11777960daf56ded8407ceab05e882b17
+size 5111
