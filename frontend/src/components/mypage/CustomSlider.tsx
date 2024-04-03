9bf36b2945ecd899991cde4098c065b421d4040a
yeonmi <ar77gt@daum.net>
2024-04-03 21:33:24 +0900

    merge
    Former-commit-id: bf7b8f73e4f87f3492089322bc908828c7cc088b

--- frontend/src/components/mypage/CustomSlider.tsx
+++ frontend/src/components/mypage/CustomSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d90a9cd26aa40b317169b70530d9d24241856d01d2626b9387434d3d6bcfc5e0
+oid sha256:4a52e3e84284b9b00b7d0872476a2fb57a25f47573dffb56cfc90d80a8bf27d7
 size 13112
