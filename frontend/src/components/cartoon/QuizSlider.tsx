d6785de088b17c74c4b86c949bb55b9027b5ff9e
Jaechan Kim <penguin_03@naver.com>
2024-04-03 21:12:00 +0900

    feat: add button
    Former-commit-id: 5c4f33b31cdefdb97f1fd4c9a408ca9bc4224afd

--- frontend/src/components/cartoon/QuizSlider.tsx
+++ frontend/src/components/cartoon/QuizSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:100644a787ada0c85b7e1970556adf3a23c0ccebac4a88c1b7d6f3cfbbd776f1
-size 9719
+oid sha256:714d6ec1f10fb04c13ce3f3769d2732d5fdfd3b3bfb4f9a495d86ae3de7aa7df
+size 10805
