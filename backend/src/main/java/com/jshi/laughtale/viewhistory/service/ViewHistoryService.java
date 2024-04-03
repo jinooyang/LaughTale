3f5175d42481f6c78c01823b518af62ea33771f1
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-03 17:24:54 +0900

    fix: ViewHistory 에러 처리
    Former-commit-id: 8995ea637d3ba216f771bd42b2623317081a238e

--- backend/src/main/java/com/jshi/laughtale/viewhistory/service/ViewHistoryService.java
+++ backend/src/main/java/com/jshi/laughtale/viewhistory/service/ViewHistoryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f67c728b5eb567c34893476c2a45cbdd0baaa01d74666051804564cf111e596
-size 1933
+oid sha256:92eb90fc84ed32acbd7becc0474e20f5f556e700d3aca14b917fba9712c40961
+size 2049
