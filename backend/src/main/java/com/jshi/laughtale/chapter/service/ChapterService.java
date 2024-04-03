ba6fc80dd5875276119d970d2445412262b961df
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-03 17:34:27 +0900

    fix: 페이지 prev, next 반대로 가는 오류 수정
    Former-commit-id: 1e15d0ab40859c0c7fcc65ae4d661ff638fecf38

--- backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:663d0747f6451ed7ee601396086cad88c225e547739350cb8387037e4d8dde42
-size 5903
+oid sha256:f6e4e183d7e2d565587df294fe2cc89b69f2ea0991eab2dc495593dab69c28f8
+size 5907
