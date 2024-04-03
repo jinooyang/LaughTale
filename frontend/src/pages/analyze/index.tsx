a67ae8a2c4c3ac332e199b3efef7e99164fc4900
yeonmi <ar77gt@daum.net>
2024-04-03 20:02:24 +0900

    merge
    Former-commit-id: 616157926d4e81c1330a70cc8ae667113c5e3d87

--- frontend/src/pages/analyze/index.tsx
+++ frontend/src/pages/analyze/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2afe2097dcaa9e638428801330ce8e58f62337938ff4d239f7f8af930b0e36dd
-size 8887
+oid sha256:16cd3243f3025476e454350b2ce7a7df0d8bcd0494239bbca512daa6d9bb9bb8
+size 8872
