5dd60bfb80e5d22f40578643b471bfb609da31c1
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-03 17:01:02 +0900

    fix: history 등록 및 prev, next
    Former-commit-id: 8b64002cc00a6712ebba9753e3c82e7f7433cfc7

--- backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
+++ backend/src/main/java/com/jshi/laughtale/chapter/service/ChapterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7d4cef3dead3b2a2a40f41abfb0c6e64ad461464e883f6b223e978833678457
-size 5805
+oid sha256:663d0747f6451ed7ee601396086cad88c225e547739350cb8387037e4d8dde42
+size 5903
