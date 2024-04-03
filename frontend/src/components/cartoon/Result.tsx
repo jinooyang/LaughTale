12604fdbf4907c9a61e5f6e8b5a86e38577df2fb
yeonmi <ar77gt@daum.net>
2024-04-03 15:31:56 +0900

    merge
    Former-commit-id: b6fe009ab925de2de5cddfaa661608e06838a923

--- frontend/src/components/cartoon/Result.tsx
+++ frontend/src/components/cartoon/Result.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e8370088c7e72878dde4da5467c04e2778f2aa23182cf6c6e8465ab4a015540
+oid sha256:5210b9d30fc670de134907f9a35d2bc8911f5f4f3486a14fa6690529d2ea1d1d
 size 4105
