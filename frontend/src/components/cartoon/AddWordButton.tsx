7c042645b088386370c37006c8045b76cd145bb4
Jaechan Kim <penguin_03@naver.com>
2024-04-03 20:30:00 +0900

    feat: add toast
    Former-commit-id: 1beef89042568c25d1644f5cba3045a044e19b65

--- frontend/src/components/cartoon/AddWordButton.tsx
+++ frontend/src/components/cartoon/AddWordButton.tsx
@@ -1,15 +1,3 @@
-import {useCallback} from "react";
-import {addWordList} from "../../apis/viewer.ts";
-
-type Props={
-  wordId: number
-}
-export const AddWordButton = (props: Props) => {
-  const onClick = useCallback(async () => {
-    const response = await addWordList(+ props.wordId);
-    console.log("add" , response)
-  }, [props.wordId]);
-  return (<button className="bg-[#73ABE5] hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full" onClick={onClick}>
-    추가
-    </button>)
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca114e076e85468a05dc005507671553003bf748ddd0ba4e99389a706c059bcd
+size 679
--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e8fd4587fa6a5006040fb9e3b422771140bcdfc7c23b1418da6777c7e89c7f7
-size 4869
+oid sha256:087b0e72aeae2fc96d54ec5c649b42391e966b7d501e1e62e7d7bd82d7427b28
+size 4935
