c490ef4bf6bd413d3dfc2b1a44eca91b8766322c
jinooyang <jinooyang@gmail.com>
2024-04-03 09:43:11 +0900

    fix : getlevelmanga order by id desc
    Former-commit-id: cc76078971bc3391d7a7901e47a012d8d0e24888

--- backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
+++ backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b79a7a0275d549a643067199ebd3b5208d0eba77fda3bb2c971bd4eba4afe3e
-size 1553
+oid sha256:b24212e93ab8667710a40a06e43ca7faf380765d2fc19856ea55581737a68e90
+size 1645
