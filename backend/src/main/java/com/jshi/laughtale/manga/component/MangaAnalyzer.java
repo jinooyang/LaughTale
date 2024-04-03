afe977d20094c7c995e525ddf78dd019d812e631
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-03 17:05:24 +0900

    fix: 챕터 레벨 반영
    Former-commit-id: 88f9ddc66bb145a265632d575126487c43af9e79

--- backend/src/main/java/com/jshi/laughtale/manga/component/MangaAnalyzer.java
+++ backend/src/main/java/com/jshi/laughtale/manga/component/MangaAnalyzer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fb051714ee3e8fe13633f6a2dbbdd3cac616253332f7948907f7686ea563c21
-size 6711
+oid sha256:1c499f64f33fcb8620fd35345bd9aeb8affa2e86b922f68235a94c25cb9a7563
+size 6746
