07b184ae1c7d79e5e37694761ab6ef1eb4e1f7cf
root <root@ip-172-26-6-78.ap-northeast-2.compute.internal>
2024-04-03 19:16:37 +0900

    feat: python api

---
+++ manga-python/manga_detector/detect.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3744bd02d946ff6481a67823b06c68c0d1e85bd01b03b8b53b4baff103d90a6
+size 2495
---
+++ manga-python/utils/image_editor.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10a47253d33f5142c8a76e4de4d9c37c1c9e1ebefa6075bfa9262841ce3c29b1
+size 323
