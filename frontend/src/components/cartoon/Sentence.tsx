345adf89834b19592b632ce096691e03fbb5063e
Jaechan Kim <penguin_03@naver.com>
2024-04-03 21:34:53 +0900

    feat add close button
    Former-commit-id: 30548150d7ce740ef1c96464368c0040c6c38b14

--- frontend/src/components/cartoon/Sentence.tsx
+++ frontend/src/components/cartoon/Sentence.tsx
@@ -1,13 +1,3 @@
-import React from "react";
-import SpeechButton from "../common/SpeechButton.tsx";
-
-type Props = {
-  sentence: string
-  originSentence : string;
-}
-export default function Sentence(props: Props){
-  return <div className="sticky top-0 bg-white p-5 flex z-50">
-    <div dangerouslySetInnerHTML={{__html:props.sentence}} className="text-6xl text-black"></div>
-    <SpeechButton sentence={props.originSentence} style={{width:"5rem", display:"inline", color:"#b26aff"}}/>
-  </div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9afbcb51b80c9c5ec357940818b3af7995e70f115caa2ed71d85dfa610c1cc11
+size 613
