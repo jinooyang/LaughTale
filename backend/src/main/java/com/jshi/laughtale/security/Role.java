2ab49ace5997164ffccbe89ce73f4f5d2f6597ce
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-05 15:16:36 +0900

    fix: 관리자, 사용자 업로드 및 분석 기능 분리
    Former-commit-id: 5cd0a27243f0309122fceb7757d20693e09f8f94

--- backend/src/main/java/com/jshi/laughtale/security/Role.java
+++ backend/src/main/java/com/jshi/laughtale/security/Role.java
@@ -1,18 +1,3 @@
-package com.jshi.laughtale.security;
-
-public enum Role {
-
-    ROLE_USER("USER"),
-    ROLE_ANONYMOUS("ANONYMOUS"),
-    ROLE_ADMIN("ADMIN");
-
-    private String role;
-
-    Role(String role) {
-        this.role = role;
-    }
-
-    public String value() {
-        return role;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:44786371be4b9b33db01a24740344811e88f556097f615af624dc42b1a0635e2
+size 370
