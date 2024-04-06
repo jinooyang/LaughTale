95cb8b004e16833a6f99175ec83d47c43718d00e
j-yong98 <chlwodyd9829@gmail.com>
2024-04-07 02:44:41 +0900

    fix: 헤더 반응형 변경
    Former-commit-id: bba869c9c3c2312ed9d3178d5292ff09304db803

--- frontend/src/components/common/BlueHeader.tsx
+++ frontend/src/components/common/BlueHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd04e6e089dc396ce590f262276c8dead118e4704b1b40ee0e79f93f6da502f7
-size 1854
+oid sha256:ca914361e8013be468c6d0173d82c89927ed76ad94831237e9cc9cf097e4db95
+size 3011
