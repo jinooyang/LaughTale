64ef0f5cfbdf3fc3cdb8f1bbe1a505e935ea418a
j-yong98 <chlwodyd9829@gmail.com>
2024-04-07 02:44:41 +0900

    fix: 헤더 반응형 변경
    Former-commit-id: c1c5516d770f6fd6aec2a8ae050f358d85024118

--- frontend/src/components/common/BlueAfterLoginButton.tsx
+++ frontend/src/components/common/BlueAfterLoginButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88701a70c34f15327029c8977b4a1d2654bd9bc2f390fd5b7e41f390ea15ca91
-size 820
+oid sha256:18f270246f6c7b0b6652f8a0669b82b6ffc6d7c47efda9dad74d5fd1f9442975
+size 2057
