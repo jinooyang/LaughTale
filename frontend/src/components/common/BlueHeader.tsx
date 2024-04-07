ccee80921eae798adf638614ecf047a78c32fd33
dohyeong <ehgud619@naver.com>
2024-04-08 08:11:30 +0900

    fix: 로고수정
    Former-commit-id: cdd77ef75b6907467d40447d4ed0aee97e279482

--- frontend/src/components/common/BlueHeader.tsx
+++ frontend/src/components/common/BlueHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a440511b4ee16ba772cf393ae069a247828f3510644bfb2344ce91bf9f0dc38e
-size 3111
+oid sha256:3c23782b0422a29005550afb2daf680f00e5dd4e1b83b7597fe5fcf539f11193
+size 3112
--- frontend/src/pages/newmain/NewMain.tsx
+++ frontend/src/pages/newmain/NewMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eb019787f47738365de31760195802868bbf5a237f8db51b5c42f642db86a53
-size 17078
+oid sha256:9e7c6554bf1f71c34b950862f7acd68b89a42b79b008f388cb92cbfc3757da59
+size 17080
