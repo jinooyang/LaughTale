2c150bc7db33a6a958c4c334bca1d422d84f508c
jaechan <penguin_03@naver.com>
2024-04-07 15:21:49 +0900

    fix: 헤더 토글 안닫히는 현상 해결
    Former-commit-id: 70554e7386f21ccd27305cc9a24f35984336080a

--- frontend/src/components/common/BlueAfterLoginButton.tsx
+++ frontend/src/components/common/BlueAfterLoginButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18f270246f6c7b0b6652f8a0669b82b6ffc6d7c47efda9dad74d5fd1f9442975
-size 2057
+oid sha256:81bb3f64bcbd634f0e5c9baa718815227ff4a0ef08f64abe3c1b4f80f36debd4
+size 2101
--- frontend/src/components/common/BlueHeader.tsx
+++ frontend/src/components/common/BlueHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca914361e8013be468c6d0173d82c89927ed76ad94831237e9cc9cf097e4db95
-size 3011
+oid sha256:a440511b4ee16ba772cf393ae069a247828f3510644bfb2344ce91bf9f0dc38e
+size 3111
