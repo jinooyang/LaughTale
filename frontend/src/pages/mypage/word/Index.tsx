0e5b0d8c7f565d739c9ff00b4bbc3d475bddc626
jaechan <penguin_03@naver.com>
2024-04-07 22:20:24 +0900

    fix: 단어 삭제 버튼 기능 수정
    Former-commit-id: 8fc2d0b7aa4969642f7d7aaf6583c8ac290555f2

--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce61dac3f13d9694cff65d02f8130ab37945437ac6cae630abd145c877e2fa8a
-size 4668
+oid sha256:c6d2355ee5453b73a418fc09d8a6fbe7bd0e8f5a8313cf33c2be4e72ffd9a25c
+size 5134
